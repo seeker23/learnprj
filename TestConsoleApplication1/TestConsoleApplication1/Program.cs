0697430bf70443d2e9afed316969bb1d3552956c
seeker23 <adrian.seeker@gmail.com>
2016-08-03 13:43:05 +0300

    dfsdfsd

--- TestConsoleApplication1/TestConsoleApplication1/Program.cs
+++ TestConsoleApplication1/TestConsoleApplication1/Program.cs
@@ -15,6 +15,8 @@ namespace TestConsoleApplication1
             Console.WriteLine("test");
             Console.WriteLine("test");
             Console.WriteLine("test");
+
+            Console.WriteLine("test");
             Console.ReadLine();
         }
     }
